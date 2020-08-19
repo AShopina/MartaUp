0abd366909ced7ed67cb91cedf3a008045c75dc5
Shopina <anna.nemchenko0902@gmail.com>
2020-08-20 02:51:09 +0300

    final v

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,51 +9,53 @@ document.querySelector('.menu__hamburger').onclick = function () {
 
 
 
-// code for slider
-const pagination = document.querySelector('.pagination');
-const points = document.querySelectorAll('.pagination-item__point');
-let pointsArray = Array.from(points);
-let counter = 0;
-let targetPoint = pointsArray[counter];
-const slides = document.querySelector('.slides-wrapper');
+{
+    // code for slider
+    const pagination = document.querySelector('.pagination');
+    const points = document.querySelectorAll('.pagination-item__point');
+    let pointsArray = Array.from(points);
+    let counter = 0;
+    let targetPoint = pointsArray[counter];
+    const slides = document.querySelector('.slides-wrapper');
 
-pagination.addEventListener('click', getTarget);
+    pagination.addEventListener('click', getTarget);
 
-function getTarget(event) {
-    targetPoint = event.target;
-    if (!targetPoint.classList.contains('pagination-item__point')) return;
-    activePoint(targetPoint);
-}
-
-function activePoint() {
-    targetPoint.classList.add('point-active');
-    points.forEach((point) => {
-        if (point != targetPoint) point.classList.remove('point-active');
-    });
-    numOfPoint(targetPoint);
-}
-
-function numOfPoint(targetPoint) {
-    let numActivePoint = pointsArray.indexOf(targetPoint);
-    slider(numActivePoint);
-}
-
-function slider(numActivePoint) {
-    let sliderLeft = -numActivePoint * 100;
-    slides.style['margin-left'] = sliderLeft + "%";
-}
-
-
-// code for animation
-function animationSlider() {
-    
-    if (counter < 5) {
-        targetPoint = pointsArray[counter++];
-        setTimeout(animationSlider, 3000);
-    } else {
-        counter = 0;
-        setTimeout(animationSlider, 3000);
+    function getTarget(event) {
+        targetPoint = event.target;
+        if (!targetPoint.classList.contains('pagination-item__point')) return;
+        activePoint(targetPoint);
     }
-    activePoint(targetPoint);
+
+    function activePoint() {
+        targetPoint.classList.add('point-active');
+        points.forEach((point) => {
+            if (point != targetPoint) point.classList.remove('point-active');
+        });
+        numOfPoint(targetPoint);
+    }
+
+    function numOfPoint(targetPoint) {
+        let numActivePoint = pointsArray.indexOf(targetPoint);
+        slider(numActivePoint);
+    }
+
+    function slider(numActivePoint) {
+        let sliderLeft = -numActivePoint * 100;
+        slides.style.marginLeft = sliderLeft + "%";
+    }
+
+
+    // code for animation
+    function animationSlider() {
+
+        if (counter < 5) {
+            targetPoint = pointsArray[counter++];
+            setTimeout(animationSlider, 3000);
+        } else {
+            counter = 0;
+            setTimeout(animationSlider, 3000);
+        }
+        activePoint(targetPoint);
+    }
+    animationSlider();
 }
-animationSlider();
